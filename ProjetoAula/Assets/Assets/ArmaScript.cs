b35031b4a7039e94f433983b511fd6298ad4510d
C1313896 Pedro Bomfim Da Costa <c1313896@interno.bb.com.br>
2020-05-11 11:38:12 -0300

    atualização

--- ProjetoAula/Assets/Assets/ArmaScript.cs
+++ ProjetoAula/Assets/Assets/ArmaScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class ArmaScript : MonoBehaviour
 {
 
-    public int municao;
+    
     public GameObject projetil;
     public GameObject projetil2;
     public GameObject positionl;
@@ -14,40 +14,46 @@ public class ArmaScript : MonoBehaviour
     public GameObject textoMunicao;
     public GameObject arma1;
     public GameObject arma2;
+    public AudioSource somTiro;
+    public int toogle = 1;
+    public int municao;
 
     private int cooldown;
-
     private int contadorCooldown;
-    Camera mainCamera;
-    public int toogle = 1;
-    private float movementSpeed = 5f;
+    private Camera mainCamera;
+    private AudioSource audio;
+
     void Start()
     {
-        this.municao = 100;
         this.textoMunicao.GetComponent<Text>().text = "Munição: "+this.municao.ToString();
         this.mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
-        this.cooldown = 10;
+        this.cooldown = 90;
         this.contadorCooldown = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        contadorCooldown++;
         if(Input.GetButtonDown("Fire1")){
-            
-            if(this.municao>0){
-                if(toogle==1){
-                    Instantiate(projetil,positionl.transform.position,positionl.transform.rotation);
-                }else{
-                    Instantiate(projetil2,position2.transform.position,position2.transform.rotation);
-                }
-                this.municao--;
-                this.textoMunicao.GetComponent<Text>().text = "Munição: "+this.municao.ToString();
-            }
-        }if(Input.GetButton("Fire1")){
-            contadorCooldown++;
             if(contadorCooldown>=cooldown){
                 if(this.municao>0){
+                    this.somTiro.Play();
+                    if(toogle==1){
+                        Instantiate(projetil,positionl.transform.position,positionl.transform.rotation);
+                    }else{
+                        Instantiate(projetil2,position2.transform.position,position2.transform.rotation);
+                    }
+                    this.municao--;
+                    this.textoMunicao.GetComponent<Text>().text = "Munição: "+this.municao.ToString();
+                }
+                contadorCooldown = 0;
+            }
+        }if(Input.GetButton("Fire1")){
+            
+            if(contadorCooldown>=cooldown){
+                if(this.municao>0){
+                    this.somTiro.Play();
                     if(toogle==1){
                         Instantiate(projetil,positionl.transform.position,positionl.transform.rotation);
                     }else{
